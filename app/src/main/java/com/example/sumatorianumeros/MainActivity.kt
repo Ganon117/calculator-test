104c12f6f61a068ce744972351cdde5024448c37
JuanAntonioGZ <jugarza_6181@hotmail.com>
2023-07-14 13:40:32 -0600

    Mensaje del commit

--- app/src/main/java/com/example/sumatorianumeros/MainActivity.kt
+++ app/src/main/java/com/example/sumatorianumeros/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : AppCompatActivity() {
 
 
         val suma = {x:Int,y:Int -> x + y}
+        val resta = {x:Int,y:Int -> x - y}
         binding.appCompatButton.setOnClickListener {
            println( suma(binding.suma1.text.toString().toInt(),binding.suma2.text.toString().toInt()))
             val intent = Intent(this@MainActivity,Main2::class.java)
